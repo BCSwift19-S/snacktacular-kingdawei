26cbdba0be973e4127a57e91b1363becd79030ff
default <ykim@fa05956kyoung-tae.bc.edu>
2019-03-31 17:15:07 -0400

    9.4 complete: Added Google Place to look up restaurants

--- Snacktacular/AppDelegate.swift
+++ Snacktacular/AppDelegate.swift
@@ -4,6 +4,7 @@
 
 import UIKit
 import Firebase
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSPlacesClient.provideAPIKey("AIzaSyDG86ivw_gczqdlQ5YFXfgY89AVMi1_zJI")
         FirebaseApp.configure()
         return true
     }
